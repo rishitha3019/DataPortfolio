23e383ef23248fefb16ccce65222b17e156361c9
rishitha3019 <kalvakolrishitha@gmail.com>
2025-06-03 17:31:18 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
+
+const App = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+};
+
+export default App;
 
