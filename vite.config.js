58b27b69663fd7040e5506a42bbf53a0e3d0cb1d
rishitha3019 <kalvakolrishitha@gmail.com>
2025-06-03 17:53:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages'
+
+export default defineConfig({
+  plugins: [react(), ghPages()],
+  base: "/your-repo-name/", // ← Replace with your GitHub repo name
+})
 
