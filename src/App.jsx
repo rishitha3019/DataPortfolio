2f0f416b058acf9e500f2e18f322d04280f8305e
rishitha3019 <kalvakolrishitha@gmail.com>
2025-06-03 17:43:34 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
 
-function App() {
+const App = () => {
   return (
-    <Layout>
-      <Home />
-      <Projects />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
-
 
